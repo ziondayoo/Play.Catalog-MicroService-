a550c54ef97419ea2467b46393a8aa6ae55d5621
ziondayoo <adetunjitemidayo26@gmail.com>
2023-01-30 12:57:46 +0100

    Second commit

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -22,9 +22,13 @@ namespace Play.Catalog.Service.Controllers
         
         //GET /items/{id}
         [HttpGet("{id}")]
-        public ItemDto GetById(Guid id)
+        public ActionResult<ItemDto> GetById(Guid id)
         {
             var item = items.Where(item => item.Id == id).SingleOrDefault();
+            if(item == null)
+            {
+                return NotFound();
+            }
             return item;
         }
         // POST
@@ -40,6 +44,10 @@ namespace Play.Catalog.Service.Controllers
         public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
         {
             var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+            if(existingItem == null)
+            {
+                return NotFound();
+            }
             var updatedItem = existingItem with 
             {
                 Name = updateItemDto.Name,
@@ -56,6 +64,10 @@ namespace Play.Catalog.Service.Controllers
         public IActionResult Delete(Guid id)
         {
             var index = items.FindIndex(existingItem => existingItem.Id == id);
+            if(index == 0)
+            {
+                return NotFound();
+            }
             items.RemoveAt(index);
             return NoContent();
         }
